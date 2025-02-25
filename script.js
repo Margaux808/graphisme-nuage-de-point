45cb21741cdcfbb82f9682c5aaf6f9b847321f62
Margaux808 <162150195+Margaux808@users.noreply.github.com>
2025-02-25 13:09:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,79 +1,200 @@
-d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json").then(function(data) {
-  const svg = d3.select("#graph");
-  const margin = { top: 20, right: 30, bottom: 50, left: 50 };
-  const width = svg.attr("width") - margin.left - margin.right;
-  const height = svg.attr("height") - margin.top - margin.bottom;
 
-  // Scales
-  const x = d3.scaleLinear().domain([d3.min(data, d => d["Year"]), d3.max(data, d => d["Year"])]).range([0, width]);
-  const y = d3.scaleTime().domain([d3.min(data, d => new Date(d["Time"])), d3.max(data, d => new Date(d["Time"]))]).range([height, 0]);
+var projectName = 'scatter-plot';
 
-  // Axes
-  const xAxis = d3.axisBottom(x).tickFormat(d3.format("d"));
-  const yAxis = d3.axisLeft(y).tickFormat(d3.timeFormat("%M:%S"));
+// RETURN TO GIDCDN link once chances propogate
 
-  svg.append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`)
-    .append("g")
-    .attr("id", "x-axis")
-    .attr("transform", `translate(0,${height})`)
-    .call(xAxis);
+// coded by @paycoguy
 
-  svg.append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`)
-    .append("g")
-    .attr("id", "y-axis")
-    .call(yAxis);
+var url =
+  'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
+var margin = {
+    top: 100,
+    right: 20,
+    bottom: 30,
+    left: 60
+  },
+  width = 920 - margin.left - margin.right,
+  height = 630 - margin.top - margin.bottom;
 
-  // Title
-  svg.append("text")
-    .attr("id", "title")
-    .attr("x", width / 2)
-    .attr("y", 20)
-    .attr("text-anchor", "middle")
-    .text("Graphique des Performances Cyclistes");
+var x = d3.scaleLinear().range([0, width]);
 
-  // Circles for data points
-  const dots = svg.append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`)
-    .selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("class", "dot")
-    .attr("cx", d => x(d["Year"]))
-    .attr("cy", d => y(new Date(d["Time"])))
-    .attr("r", 5)
-    .attr("data-xvalue", d => d["Year"])
-    .attr("data-yvalue", d => new Date(d["Time"]))
-    .style("fill", "#ff6600");
+var y = d3.scaleTime().range([0, height]);
 
-  // Tooltip
-  const tooltip = d3.select("#tooltip");
+var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-  dots.on("mouseover", function(event, d) {
-    tooltip.transition().duration(200).style("opacity", 0.9);
-    tooltip.html(d["Name"] + ": " + d["Nationality"] + "<br>" + d["Year"] + " - " + d["Time"])
-      .attr("data-year", d["Year"])
-      .style("left", event.pageX + 5 + "px")
-      .style("top", event.pageY - 28 + "px");
+var timeFormat = d3.timeFormat('%M:%S');
+var xAxis = d3.axisBottom(x).tickFormat(d3.format('d'));
+
+var yAxis = d3.axisLeft(y).tickFormat(timeFormat);
+
+// Define the div for the tooltip
+var div = d3
+  .select('body')
+  .append('div')
+  .attr('class', 'tooltip')
+  .attr('id', 'tooltip')
+  .style('opacity', 0);
+
+var svg = d3
+  .select('body')
+  .append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .attr('class', 'graph')
+  .append('g')
+  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+d3.json(url)
+  .then(data => {
+    data.forEach(function (d) {
+      d.Place = +d.Place;
+      var parsedTime = d.Time.split(':');
+      d.Time = new Date(1970, 0, 1, 0, parsedTime[0], parsedTime[1]);
+    });
+
+    x.domain([
+      d3.min(data, function (d) {
+        return d.Year - 1;
+      }),
+      d3.max(data, function (d) {
+        return d.Year + 1;
+      })
+    ]);
+    y.domain(
+      d3.extent(data, function (d) {
+        return d.Time;
+      })
+    );
+
+    svg
+      .append('g')
+      .attr('class', 'x axis')
+      .attr('id', 'x-axis')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(xAxis)
+      .append('text')
+      .attr('class', 'x-axis-label')
+      .attr('x', width)
+      .attr('y', -6)
+      .style('text-anchor', 'end')
+      .text('Year');
+
+    svg
+      .append('g')
+      .attr('class', 'y axis')
+      .attr('id', 'y-axis')
+      .call(yAxis)
+      .append('text')
+      .attr('class', 'label')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 6)
+      .attr('dy', '.71em')
+      .style('text-anchor', 'end')
+      .text('Best Time (minutes)');
+
+    svg
+      .append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('x', -160)
+      .attr('y', -44)
+      .style('font-size', 18)
+      .text('Time in Minutes');
+
+    svg
+      .selectAll('.dot')
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('class', 'dot')
+      .attr('r', 6)
+      .attr('cx', function (d) {
+        return x(d.Year);
+      })
+      .attr('cy', function (d) {
+        return y(d.Time);
+      })
+      .attr('data-xvalue', function (d) {
+        return d.Year;
+      })
+      .attr('data-yvalue', function (d) {
+        return d.Time.toISOString();
+      })
+      .style('fill', function (d) {
+        return color(d.Doping !== '');
+      })
+      .on('mouseover', function (event, d) {
+        div.style('opacity', 0.9);
+        div.attr('data-year', d.Year);
+        div
+          .html(
+            d.Name +
+              ': ' +
+              d.Nationality +
+              '<br/>' +
+              'Year: ' +
+              d.Year +
+              ', Time: ' +
+              timeFormat(d.Time) +
+              (d.Doping ? '<br/><br/>' + d.Doping : '')
+          )
+          .style('left', event.pageX + 'px')
+          .style('top', event.pageY - 28 + 'px');
+      })
+      .on('mouseout', function () {
+        div.style('opacity', 0);
+      });
+
+    // title
+    svg
+      .append('text')
+      .attr('id', 'title')
+      .attr('x', width / 2)
+      .attr('y', 0 - margin.top / 2)
+      .attr('text-anchor', 'middle')
+      .style('font-size', '30px')
+      .text('Doping in Professional Bicycle Racing');
+
+    // subtitle
+    svg
+      .append('text')
+      .attr('x', width / 2)
+      .attr('y', 0 - margin.top / 2 + 25)
+      .attr('text-anchor', 'middle')
+      .style('font-size', '20px')
+      .text("35 Fastest times up Alpe d'Huez");
+
+    var legendContainer = svg.append('g').attr('id', 'legend');
+
+    var legend = legendContainer
+      .selectAll('#legend')
+      .data(color.domain())
+      .enter()
+      .append('g')
+      .attr('class', 'legend-label')
+      .attr('transform', function (d, i) {
+        return 'translate(0,' + (height / 2 - i * 20) + ')';
+      });
+
+    legend
+      .append('rect')
+      .attr('x', width - 18)
+      .attr('width', 18)
+      .attr('height', 18)
+      .style('fill', color);
+
+    legend
+      .append('text')
+      .attr('x', width - 24)
+      .attr('y', 9)
+      .attr('dy', '.35em')
+      .style('text-anchor', 'end')
+      .text(function (d) {
+        if (d) {
+          return 'Riders with doping allegations';
+        } else {
+          return 'No doping allegations';
+        }
+      });
   })
-  .on("mouseout", function() {
-    tooltip.transition().duration(200).style("opacity", 0);
-  });
+  .catch(err => console.log(err));
 
-  // Add data to the table
-  const tableBody = d3.select("#data-table tbody");
-  data.forEach(d => {
-    const row = tableBody.append("tr");
-    row.append("td").text(d["Name"]);
-    row.append("td").text(d["Nationality"]);
-    row.append("td").text(d["Year"]);
-    row.append("td").text(d["Time"]);
-  });
-
-  // Legend
-  const legend = d3.select("#legend");
-  legend.text("Légende: Points représentant les performances des cyclistes.");
-
-});
